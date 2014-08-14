276c5b811799a8ee1ae9b91cc573085c45523e58
hfcc8685 <hfcc8685@gmail.com>
2014-08-14 14:59:10 +0800

    需要的功能已完成

---
+++ README.md
@@ -0,0 +1,8 @@
+nodejs : v0.10.26  
+node-webkit : v0.8.5  
+jQuery v2.1.0  
+Bootstrap v3.1.1 
+fs-extra
+
+
+同步文件夹小工具
--- js/userEventHandle.js
+++ js/userEventHandle.js
@@ -1,14 +1,24 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
-var os = require('os');
 var gui = require('nw.gui');
 var win = gui.Window.get();
-var ncp = require('ncp').ncp;
 
-ncp.limit = 1000;
 var ConfigFilePath = path.join(path.dirname(process.execPath),"config.json");
 
 $(function(){
+
+	fs.readFile(ConfigFilePath,function (err, data){
+		if(err){
+			showError(err);
+		}		
+		else{
+			var config = JSON.parse(data);
+			$.each(config.pathPair,function(){				
+				AddSyncNode(this.sourcePath,this.targetPath);				
+			});			
+		}
+	});
+
 	$(window).on('focus', function () {
 		$("#nav-title").css("background-color","#222222");
   	});
@@ -29,6 +39,24 @@ $(function(){
 		var sourcePath = $("#tb-Source").val();
 		var targetPath = $("#tb-Target").val();
 		AddSyncNode(sourcePath,targetPath);
+
+		fs.readFile(ConfigFilePath,function (err, data){
+			if(err){
+				showError(err);
+			}		
+			else{
+				var config = JSON.parse(data);
+				var tempPathPair = {
+					"sourcePath": sourcePath,
+					"targetPath": targetPath
+				}
+				config.pathPair.push(tempPathPair);
+				fs.writeFile(ConfigFilePath,JSON.stringify(config),function(err){
+					if(err)
+						showError(err.toString());
+				});	
+			}
+		});
 	});
 });
 
@@ -52,19 +80,38 @@ function AddSyncNode(sourcePath,targetPath) {
 	targetli.appendChild(document.createTextNode(targetPath));
 
 	var syncBtnli = document.createElement("li");
-	syncBtnli.className = "list-group-item";
+	syncBtnli.className = "list-group-item clearfix";
 
 	var syncBtn = document.createElement("BUTTON");
-	syncBtn.className = "btn btn-success";
-	var btnSpan = document.createElement("span");
-	btnSpan.className = "glyphicon glyphicon-transfer";
+	syncBtn.className = "btn btn-success pull-left";
+	syncBtn.setAttribute("data-loading-text", "同步中......");
+	syncBtn.setAttribute("type","button");
+	var btnSpan = document.createElement("span");	
+	btnSpan.className = "glyphicon glyphicon-open";
 	syncBtn.appendChild(btnSpan);
 	syncBtn.appendChild(document.createTextNode(" 正向同步"));
-	syncBtn.addEventListener("click", function(){		
-		 SyncFolder(sourcePath,targetPath);	
+	syncBtn.addEventListener("click", function(){						
+		var btn = $(this)
+    	btn.button('loading')
+		SyncFolder(sourcePath,targetPath,btn);	
 	} , false);
 
+	var reverseSyncBtn = document.createElement("BUTTON");
+	reverseSyncBtn.className = "btn btn-danger pull-right";
+	reverseSyncBtn.setAttribute("data-loading-text", "同步中......");
+	reverseSyncBtn.setAttribute("type","button");
+	var reverseSyncBtnSpan = document.createElement("span");
+	reverseSyncBtnSpan.className = "glyphicon glyphicon-save";
+	reverseSyncBtn.appendChild(reverseSyncBtnSpan);
+	reverseSyncBtn.appendChild(document.createTextNode(" 逆向同步"));
+	reverseSyncBtn.addEventListener("click", function(){					
+		var btn = $(this)
+    	btn.button('loading')
+		SyncFolder(targetPath,sourcePath,btn);
+	},false);
+
  	syncBtnli.appendChild(syncBtn);
+ 	syncBtnli.appendChild(reverseSyncBtn);
 
 	ul.appendChild(sourceli);
 	ul.appendChild(targetli);
@@ -76,13 +123,57 @@ function AddSyncNode(sourcePath,targetPath) {
  	$("#rowContainer").append(row);
 }
 
-function SyncFolder(sourcePath,targetPath)
-{
-	ncp(sourcePath, targetPath, function (err) {
-		if (err) {
-   		return alert(err);
- 		}
- 		alert('done!');
+function SyncFolder(sourcePath,targetPath,btn){
+	fs.remove(targetPath,function(err){		
+		if(err){
+			btn.button('reset');
+			showError(err.toString());
+			return;
+		}
+
+		fs.copy(sourcePath,targetPath,function(copyErr){
+			btn.button('reset');
+			if(copyErr){
+				showError(copyErr.toString());
+				return;
+			}
+			showSuccess("同步成功");
+		});
 	});
 }
 
+function showSuccess(message){
+	BootstrapDialog.show({
+				title: '成功',
+				message: message,
+				type: BootstrapDialog.TYPE_SUCCESS,
+				buttons: [{
+					id: 'btn-ok',
+					icon: 'glyphicon glyphicon-check',
+					label: 'OK',
+					cssClass : 'btn-success',
+					autospin: false,
+					action: function(dialogRef) {
+						dialogRef.close();
+					}
+				}]
+			});
+}
+
+function showError(err){
+	BootstrapDialog.show({
+				title: '失败',
+				message: err,
+				type: BootstrapDialog.TYPE_SUCCESS,
+				buttons: [{
+					id: 'btn-ok',
+					icon: 'glyphicon glyphicon-check',
+					label: 'OK',
+					cssClass : 'btn-success',
+					autospin: false,
+					action: function(dialogRef) {
+						dialogRef.close();
+					}
+				}]
+			});	
+}
